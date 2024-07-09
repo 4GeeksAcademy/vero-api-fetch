c79a5cf1ffacaf43540a241916d432b7bc4c12b8
Vero519 <voviedo6@gmail.com>
2024-07-09 20:22:16 +0000

    primer-api-fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,90 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	//declaracion de estados
+	let [tarea, setTarea] = useState("")
+	let [lista, setLista] = useState([])
+
+	function crearUsuario (){
+
+		fetch('https://playground.4geeks.com/todo/users/Vero519',{
+			method:'POST',
+			headers: {
+				"Content-Type" :"application/json"
+			}
+
+		})
+		
+		.then((respose)=> respose.json())
+		.then((data)=> console.log(data))
+		.catch((error)=> console.log(error))
+	}
+
+	function obtenerTareas (){
+
+		fetch('https://playground.4geeks.com/todo/users/Vero519',{
+			method:'GET',
+			headers: {
+				"Content-Type" :"application/json"
+			}
+
+		})
+		
+		.then((respose)=> respose.json())
+		.then((data)=> setLista(data.todos))
+		.catch((error)=> console.log(error))
+	}
+
+	const agregarTarea = () => {
+		if (tarea.trim() !== "") {
+			const nuevaLista = {
+				id: tarea.length + 1,
+				texto: tarea
+			};
+
+			setLista([...lista, nuevaLista]);
+			setTarea("");
+		}
+	}
+	const eliminarTarea = (id) => {
+		setLista(lista.filter(elemento => elemento.id !== id));
+	}
+useEffect(()=>{
+	crearUsuario()
+	obtenerTareas()
+},[])
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+
+		<div className="container">
+			<div className="text-center">
+
+				<label htmlFor="inputValue" className="form-label fs-1 text-secondary">todos</label>
+				<div className="d-flex">
+					<input type="text" id="inputValue" className="form-control fs-3" value={tarea} onChange={(e) => setTarea(e.target.value)} onKeyDown={(e) => e.key == "Enter" && agregarTarea()} />
+					<button type="button" className="btn btn-primary" onClick={agregarTarea}>Agrgar a la lista</button>
+				</div>
+				<div className="card fs-3 text-start">
+					<ul className="list-group list-group-flush">
+						{lista.map(item => (
+							<li className="list-group-item" key={item.id}>
+								<span >{item.label}</span>
+								<button className="btn btn-ligth float-end" onClick={() => eliminarTarea(item.id)}>x</button>
+							</li>
+						))}
+					</ul>
+				</div>
+				<div id="passwordHelpBlock" className="form-text">
+					<ul>
+
+					</ul>
+				</div>
+
+			</div>
 		</div>
 	);
 };
