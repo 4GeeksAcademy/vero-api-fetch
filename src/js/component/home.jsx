9155121d18c1e32fcc1bdbd13e1ffaea635d2bd3
Vero519 <voviedo6@gmail.com>
2024-07-10 15:18:14 +0000

    error

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 
 //create your first component
@@ -6,59 +6,95 @@ const Home = () => {
 	//declaracion de estados
 	let [tarea, setTarea] = useState("")
 	let [lista, setLista] = useState([])
+}
 
-	function crearUsuario (){
 
-		fetch('https://playground.4geeks.com/todo/users/Vero519',{
-			method:'POST',
-			headers: {
-				"Content-Type" :"application/json"
+function crearUsuario() {
+
+	fetch('https://playground.4geeks.com/todo/users/Vero519', {
+		method: 'POST',
+		headers: {
+			"Content-Type": "application/json"
+		},
+
+	})
+
+		.then((respose) => {
+			if (respose.status === 201) {
+				obtenerTareas()
 			}
-
+			return respose.json()
 		})
-		
-		.then((respose)=> respose.json())
-		.then((data)=> console.log(data))
-		.catch((error)=> console.log(error))
-	}
+		.then((data) => console.log(data))
+		.catch((error) => console.log(error))
+}
 
-	function obtenerTareas (){
+function guardarTareas() {
 
-		fetch('https://playground.4geeks.com/todo/users/Vero519',{
-			method:'GET',
-			headers: {
-				"Content-Type" :"application/json"
+	fetch('https://playground.4geeks.com/todo/users/Vero519', {
+		method: 'POST',
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			"label": tarea,
+			"is_done": false
+		})
+
+	})
+
+		.then((respose) => {
+			if (respose.status === 201) {
+				obtenerTareas()
 			}
-
+			return respose.json()
 		})
-		
-		.then((respose)=> respose.json())
-		.then((data)=> setLista(data.todos))
-		.catch((error)=> console.log(error))
-	}
+	
+	
+		.then((data) => console.log(data))
+		.catch((error) => console.log(error))
+}
 
-	const agregarTarea = () => {
-		if (tarea.trim() !== "") {
-			const nuevaLista = {
-				id: tarea.length + 1,
-				texto: tarea
-			};
+function obtenerTareas() {
 
-			setLista([...lista, nuevaLista]);
-			setTarea("");
+	fetch('https://playground.4geeks.com/todo/users/Vero519'), {
+		method: 'GET',
+		headers: {
+			"Content-Type": "application/json"
 		}
+
+			.then((respose) => respose.json())
+			.then((data) => setLista(data.todos))
+			.catch((error) => console.log(error))
 	}
-	const eliminarTarea = (id) => {
-		setLista(lista.filter(elemento => elemento.id !== id));
+	
+	function EliminarDesdeApi(id) {
+		fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+			method: 'DELETE',
+			headers: {
+				"Content-Type": "application/json"
+			}
+
+		})
+
+			.then((respose) => respose.json())
+			.then((data) => console.log(data))
+			.catch((error) => console.log(error))
+
 	}
-useEffect(()=>{
-	crearUsuario()
+
+
+	//const eliminarTarea = (id) => {
+	//	setLista(lista.filter(elemento => elemento.id !== id));
+	//}
+
+	useEffect(() => {
+	//crearUsuario()
 	obtenerTareas()
-},[])
+	}, [])
 
 
 	return (
-
 
 		<div className="container">
 			<div className="text-center">
@@ -66,14 +102,14 @@ useEffect(()=>{
 				<label htmlFor="inputValue" className="form-label fs-1 text-secondary">todos</label>
 				<div className="d-flex">
 					<input type="text" id="inputValue" className="form-control fs-3" value={tarea} onChange={(e) => setTarea(e.target.value)} onKeyDown={(e) => e.key == "Enter" && agregarTarea()} />
-					<button type="button" className="btn btn-primary" onClick={agregarTarea}>Agrgar a la lista</button>
+					<button type="button" className="btn btn-primary" onClick={guardarTareas}>Agrgar a la lista</button>
 				</div>
 				<div className="card fs-3 text-start">
 					<ul className="list-group list-group-flush">
 						{lista.map(item => (
 							<li className="list-group-item" key={item.id}>
 								<span >{item.label}</span>
-								<button className="btn btn-ligth float-end" onClick={() => eliminarTarea(item.id)}>x</button>
+								<button className="btn btn-ligth float-end" onClick={() => EliminarDesdeApi(item.id)}>x</button>
 							</li>
 						))}
 					</ul>
